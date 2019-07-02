360a4ae675e88907e58e4b7ebc482110fffed2eb
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-07-02 16:53:52 -0700

    jwt and error handling middleware, auth/user endpoints

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
-/* eslint-disable no-console */
-'use strict';
+require('dotenv').config()
 
+const knex = require('knex')
 const app = require('./app');
-const { PORT } = require('./config');
+const { PORT, DB_URL } = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+})
+
+app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
