abe46072de0bc987d3a3bef83e2a45e5da0a7b13
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-07-02 13:34:32 -0700

    fixed game table

--- seeds/seed.squad-up.sql
+++ seeds/seed.squad-up.sql
@@ -6,10 +6,10 @@ INSERT INTO users (id, username, password, name, avatar) VALUES (
     'https://image.flaticon.com/icons/svg/78/78373.svg'
 );
 INSERT INTO games (id, game_title, game_type, image) VALUES 
-( '1', 'monopoly', 'Board Games','aaa' ),
-( '2', 'apex', 'Video Games', 'bbb' ),
-( '3', 'tennis', 'Recreation', 'ccc' ),
-( '4', 'uno', 'Card Games', 'ddd' );
+( '1', 'monopoly', 'Board Games','https://image.flaticon.com/icons/svg/78/78373.svg' ),
+( '2', 'apex', 'Video Games', 'https://image.flaticon.com/icons/svg/78/78373.svg' ),
+( '3', 'tennis', 'Recreation', 'https://image.flaticon.com/icons/svg/78/78373.svg' ),
+( '4', 'uno', 'Card Games', 'https://image.flaticon.com/icons/svg/78/78373.svg' );
 INSERT INTO squads (id, squad_name, squad_location, game_id, leader, capacity) VALUES (
     '1',
     'squad1',
