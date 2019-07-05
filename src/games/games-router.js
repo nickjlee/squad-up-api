2089e7fdb70f989f78044000e266bd78d55f2632
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-07-05 05:12:26 -0700

    games router + games service

--- src/games/games-router.js
+++ src/games/games-router.js
@@ -1,8 +1,68 @@
 const express = require('express')
-const path = require('path')
+const {requireAuth} = require('../middleware/jwt-auth')
 const GamesService = require('./games-service')
+const path = require('path')
 
 const gamesRouter = express.Router()
 const jsonParser = express.json()
 
+gamesRouter
+  .route('/')
+  .get(async (req, res, next) => {
+    try {
+      const games = await GamesService.getAllGames(req.app.get('db'))
 
+      return res.json(games)
+
+    } catch (error) {
+      next(error)
+    }
+  })
+
+gamesRouter
+  .route('/:game_id')
+  .all(requireAuth)
+  .all(checkGameExists)
+  .get((req, res) => {
+    res.json(res.game)
+  })
+
+gamesRouter
+  .route('/:game_id/squads/')
+  .all(requireAuth)
+  .all(checkGameExists)
+  .get(async (req, res, next) => {
+    try {
+      const gameSquads = await GamesService.getSquadsForGame(
+        req.app.get('db'),
+        req.params.game_id
+      )
+
+      return res.json(gameSquads)
+      
+    } catch (error) {
+      next(error)
+    }
+  })
+
+
+async function checkGameExists(req, res, next) {
+  try {
+    const game = await GamesService.getGameById(
+      req.app.get('db'),
+      req.params.game_id
+    )
+
+    if (!game)
+      return res.status(404).json({
+        error: `Game does not exist`
+      })
+
+    res.game = game
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = gamesRouter
