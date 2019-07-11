ec4f935dac292215f6164116962b6cfc569ed292
Cody Gillette <cgillette1312@gmail.com>
2019-07-11 16:53:15 -0700

    updated seeds

--- seeds/seed.squad-up.sql
+++ seeds/seed.squad-up.sql
@@ -39,17 +39,17 @@ INSERT INTO games (game_title, game_type, image) VALUES
 ( 'football', 'Recreation', 'https://image.flaticon.com/icons/svg/78/78373.svg' ),
 ( 'the awkward storyteller', 'Card Games', 'https://image.flaticon.com/icons/svg/78/78373.svg' );
 
-INSERT INTO squads (squad_name, squad_location, game_id, leader, capacity) VALUES 
-( 'squad1', 'somewhere on earth', '1', '1', '4' ),
-( 'squad2', 'somewhere on earth', '1', '1', '4' ),
-( 'random long squad name for testing', 'somewhere on earth', '1', '1', '4' ),
-( 'squad4', 'somewhere on earth', '1', '1', '4' ),
-( 'squad5', 'somewhere on earth', '1', '1', '4' ),
-( 'squad6', 'somewhere on earth', '1', '1', '4' ),
-( 'squad7', 'somewhere on earth', '1', '1', '4' ),
-( 'squad8', 'somewhere on earth', '1', '1', '4' ),
-( 'squad9', 'somewhere on earth', '1', '1', '4' ),
-( 'squad10', 'Los Angeles, CA', '1', '1', '5' );
+INSERT INTO squads (id ,squad_name, squad_location, game_id, leader, capacity) VALUES 
+('1','squad1', 'somewhere on earth', '1', '1', '4' ),
+('2','squad2', 'somewhere on earth', '1', '1', '4' ),
+('3','random long squad name for testing', 'somewhere on earth', '1', '1', '4' ),
+('4','squad4', 'somewhere on earth', '1', '1', '4' ),
+('5','squad5', 'somewhere on earth', '1', '1', '4' ),
+('6','squad6', 'somewhere on earth', '1', '1', '4' ),
+('7','squad7', 'somewhere on earth', '1', '1', '4' ),
+('8','squad8', 'somewhere on earth', '1', '1', '4' ),
+('9','squad9', 'somewhere on earth', '1', '1', '4' ),
+('10','squad10', 'Los Angeles, CA', '1', '1', '5' );
 
 INSERT INTO user_squads (user_id, squad_id) VALUES (
     '1',
