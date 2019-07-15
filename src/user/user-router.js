cbb7be2be5bab720547ff059a93c4ad569cb4663
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-07-15 13:57:59 -0700

    GET /user/:user_id completed

--- src/user/user-router.js
+++ src/user/user-router.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const path = require('path')
 const UserService = require('./user-service')
+const {requireAuth} = require('../middleware/jwt-auth')
 
 const userRouter = express.Router()
 const jsonParser = express.json()
+
+userRouter
+  .route('/:user_id')
+  .all(requireAuth)
+  .get(async (req, res, next) => {
+    try {
+      const user = await UserService.getUserInfo(
+        req.app.get('db'),
+        req.params.user_id
+      )
+
+      if (!user) 
+        return res.status(404).json({
+          error: `User does not exist`
+        })
+
+      return res.json(user)
+      
+    } catch (error) {
+      next(error)
+    }
+  })
 
 userRouter
   .post('/', jsonParser, async (req, res, next) => {
