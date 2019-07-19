c08b632d06bad74ea3681c990c39b3522d7e52e7
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-07-18 22:59:21 -0700

    added chat test

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://squadup@localhost/squadup',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://squadup@localhost/squadup-test',
+  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/squadup',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/squad-up-test',
   JWT_SECRET: process.env.JWT_SECRET || 'ocarina of time',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
