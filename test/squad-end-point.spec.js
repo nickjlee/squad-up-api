d0906bb0ff524572ba080e6c567424f74aaa11ef
Nick Lee <leexnick@gmail.com>
2019-07-19 01:11:15 -0700

    add test for / and /api/squads

--- test/squad-end-point.spec.js
+++ test/squad-end-point.spec.js
@@ -1,43 +1,57 @@
 'use strict'
 /*global supertest */
-require('./setup');
-const helpers = require('./test-helpers');
-const knex = require('knex');
-const app =require('../src/app');
-const jwt = require('jsonwebtoken');
-const config = require('../src/config');
+require('./setup')
+const helpers = require('./test-helpers')
+const knex = require('knex')
+const app = require('../src/app')
 
-describe('Squad Endpoints', () => {
-    let db;
-    before('connect to db', () => {
-        db= knex({
-            client:'pg',
-            connection:config.TEST_DB_URL
-        })
-        app.set('db', db)
-    })
-    after('disconnect from db', () => db.destroy());
-    before('cleanup', () => helpers.cleanTables(db));
-    afterEach('cleanup', () => helpers.cleanTables(db));
+describe.only('Squad Endpoints', () => {
+  let db
 
-    describe('get /api/squads', () => {
-        beforeEach('seed users and games', () => {
-            const users = helpers.makeUsersArray()
-            const games = helpers.makeGameArray()
-            helpers.seedGamesAndUsers(db, users, games)
-        })
-        it('response with squad lists empty squad list when not seeded', () => {
-            return supertest(app)
-                .get('/api/squads')
-                .expect(200, {message: `No squads joined yet!`})
-        })
-        context('seeded squads', () => {
-            const expected = helpers.makeSquadList()
-            it('response with squad list', () => {
-                return supertest(app)
-                    .get('api/squads')
-                    .expect(200, expected)
-            })
-        })
+  const testUsers = helpers.makeUsersArray()
+  const testGames = helpers.makeGameArray()
+  const testSquads = helpers.makeSquadList()
+  const testUser = testUsers[0]
+
+  before('connect to db', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL
     })
+    app.set('db', db)
+  })
+
+  after('disconnect from db', () => db.destroy())
+  before('cleanup', () => helpers.cleanTables(db))
+  afterEach('cleanup', () => helpers.cleanTables(db))
+
+  describe('GET /api/squads', () => {
+    context('Given no squads', () => {
+      beforeEach('seed users and games', () =>
+        helpers.seedGamesUsers(db, testGames, testUsers)
+      )
+
+      it('response with squad lists empty squad list when not seeded', () => {
+        return supertest(app)
+          .get('/api/squads')
+          .set('authorization', helpers.makeAuthHeader(testUser))
+          .expect(200, [])
+      })
+    })
+
+    context('Given seeded squads', () => {
+      beforeEach('seed users and squads', () =>
+        helpers.seedGamesUsersSquads(db, testGames, testUsers, testSquads)
+      )
+
+      const expected = helpers.makeExpectedSquadsList(testUser, testSquads)
+
+      it('response with squad list', () => {
+        return supertest(app)
+          .get('/api/squads')
+          .set('authorization', helpers.makeAuthHeader(testUser))
+          .expect(200, expected)
+      })
+    })
+  })
 })
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,6 +1,7 @@
-'use strict';
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+'use strict'
+const config = require('../src/config')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 function makeUsersArray() {
   return [
@@ -9,113 +10,113 @@ function makeUsersArray() {
       name: 'Test user 1',
       username: 'test-user-1',
       password: 'password',
-      xp:0,
-      level_id:1,
-      avatar:'https://image.flaticon.com/icons/svg/78/78373.svg'
+      xp: 0,
+      avatar: 'https://image.flaticon.com/icons/svg/78/78373.svg'
     },
     {
-        id: 2,
-        name: 'Test user 2',
-        username: 'test-user-2',
-        password: 'password',
-        xp:200,
-        level_id:1,
-        avatar:'https://image.flaticon.com/icons/svg/78/78373.svg'
+      id: 2,
+      name: 'Test user 2',
+      username: 'test-user-2',
+      password: 'password',
+      xp: 200,
+      avatar: 'https://image.flaticon.com/icons/svg/78/78373.svg'
     },
     {
-        id: 3,
-        name: 'Test user 3',
-        username: 'test-user-3',
-        password: 'password',
-        xp:400,
-        level_id:1,
-        avatar:'https://image.flaticon.com/icons/svg/78/78373.svg'
+      id: 3,
+      name: 'Test user 3',
+      username: 'test-user-3',
+      password: 'password',
+      xp: 400,
+      avatar: 'https://image.flaticon.com/icons/svg/78/78373.svg'
     },
     {
-        id: 4,
-        name: 'Test user 4',
-        username: 'test-user-4',
-        password: 'password',
-        xp:0,
-        level_id:2,
-        avatar:'https://image.flaticon.com/icons/svg/78/78373.svg'
-    },
-  ];
+      id: 4,
+      name: 'Test user 4',
+      username: 'test-user-4',
+      password: 'password',
+      xp: 0,
+      avatar: 'https://image.flaticon.com/icons/svg/78/78373.svg'
+    }
+  ]
 }
+
 function makeGameArray() {
   return [
     {
-      game_title:"testgame1",
-      game_type:"test1",
-      image:"testimage1",
-      id:1,
-      no_squads:2
+      game_title: 'testgame1',
+      game_type: 'Video Games',
+      image: 'testimage1',
+      id: 1
     },
     {
-      game_title:"testgame2",
-      game_type:"test1",
-      image:"testimage1",
-      id:2,
-      no_squads:0
+      game_title: 'testgame2',
+      game_type: 'Board Games',
+      image: 'testimage1',
+      id: 2
     },
     {
-      game_title:"testgame3",
-      game_type:"test2",
-      image:"testimage1",
-      id:3,
-      no_squads:1
+      game_title: 'testgame3',
+      game_type: 'Card Games',
+      image: 'testimage1',
+      id: 3
     },
     {
-      game_title:"testgame4",
-      game_type:"test3",
-      image:"testimage1",
-      id:4,
-      no_squads:1
-    },
+      game_title: 'testgame4',
+      game_type: 'Table Top Games',
+      image: 'testimage1',
+      id: 4
+    }
   ]
 }
-function makeSquadList(){
+
+function makeSquadList() {
   return [
     {
-      squad_name:"testSquad1",
-      squad_location:"testLocation1",
-      game_id:1,
-      leader:1,
-      capacity:4
+      id: 1,
+      squad_name: 'testSquad1',
+      squad_location: 'testLocation1',
+      game_id: 1,
+      leader: 1,
+      capacity: 4
     },
     {
-      squad_name:"testSquad2",
-      squad_location:"testLocation2",
-      game_id:1,
-      leader:2,
-      capacity:4
+      id: 2,
+      squad_name: 'testSquad2',
+      squad_location: 'testLocation2',
+      game_id: 1,
+      leader: 2,
+      capacity: 4
     },
     {
-      squad_name:"testSquad3",
-      squad_location:"testLocation3",
-      game_id:3,
-      leader:3,
-      capacity:4
+      id: 3,
+      squad_name: 'testSquad3',
+      squad_location: 'testLocation3',
+      game_id: 3,
+      leader: 3,
+      capacity: 4
     },
     {
-      squad_name:"testSquad4",
-      squad_location:"testLocation4",
-      game_id:4,
-      leader:2,
-      capacity:4
+      id: 4,
+      squad_name: 'testSquad4',
+      squad_location: 'testLocation4',
+      game_id: 4,
+      leader: 4,
+      capacity: 4
     }
   ]
 }
 
 function seedUsers(db, users) {
   const preppedUsers = users.map(user => {
-    return {...user, password: bcrypt.hashSync(user.password, 1)};
-  });
-  return db('users').insert(preppedUsers)
+    return { ...user, password: bcrypt.hashSync(user.password, 1) }
+  })
+  return db('users')
+    .insert(preppedUsers)
     .then(() => {
-      db.raw('SELECT setval("users_id_seq", ?)', [users[users.length-1].id]);
-    });
+      db.raw('SELECT setval("users_id_seq", ?)', [users[users.length - 1].id])
+    })
 }
+
 function cleanTables(db) {
   return db.raw(
     `TRUNCATE
@@ -125,48 +126,75 @@ function cleanTables(db) {
       user_squads
       RESTART IDENTITY CASCADE
     `
-  );
-}
-function seedGames(db, games){
-  return db('games').insert(games)
-    .then(() => {
-      db.raw('SELECT setval("games_id_seq", ?', [games[games.length-1].id]);
-    })
-}
-function seedSquads(db, squads){
-  return db('squads').insert(squads)
-  .then(
-    db.raw('SELECT setval("squads_id_seq", ?', [squads[squads.length-1].id])
   )
 }
 
-function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
-    const payload = {
-        user_id:user.id,
-        name:user.name,
-        avatar:user.avatar,
-        xp:user.xp,
-        level:user.level_id
-    }
-  const token = jwt.sign({ user_id: user.id }, secret, {
-    subject: payload,
-    algorithm: 'HS256',
-  });
-  return `Bearer ${token}`;
+function seedGames(db, games) {
+  return db('games')
+    .insert(games)
+    .then(() => {
+      db.raw('SELECT setval("games_id_seq", ?', [games[games.length - 1].id])
+    })
 }
 
-function seedGamesAndUsers(db, users, games){
-  return db.transaction(async () =>{
-    await seedUsers(db, users);
-    await seedGames(db,games);
+function seedSquads(db, squads) {
+  return db('squads')
+    .insert(squads)
+    .then(
+      db.raw('SELECT setval("squads_id_seq", ?', [squads[squads.length - 1].id])
+    )
+}
+
+function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
+  const token = jwt.sign({ user_id: user.id }, secret, {
+    subject: user.username,
+    algorithm: 'HS256'
+  })
+
+  return `Bearer ${token}`
+}
+
+function seedGamesUsers(db, games, users) {
+  return db.transaction(async () => {
+    await seedGames(db, games)
+    await seedUsers(db, users)
   })
 }
 
-function seedGameSquadUsers(db, users, games, squads){
+function seedGamesUsersSquads(db, games, users, squads) {
   return db.transaction(async () => {
-    await seedUsers(db, users);
-    await seedGames(db, games);
-    await seedSquads(db, squads);
+    await seedGames(db, games)
+    await seedUsers(db, users)
+    await seedSquads(db, squads)
+    await seedUserSquads(db, users[0], squads)
+  })
+}
+
+function seedUserSquads(db, user, squads) {
+  const userSquads = squads.map((squad, index) => {
+    return {
+      user_id: user.id,
+      squad_id: squad.id
+    }
+  })
+
+  return db('user_squads')
+    .insert(userSquads)
+}
+
+function makeExpectedSquadsList(user, squads) {
+  return squads.map(squad => {
+    return  {
+      game_id: squad.game_id,
+      leader: squad.leader,
+      name: user.name,
+      squad_id: squad.id,
+      squad_location: squad.squad_location,
+      squad_name: squad.squad_name,
+      userAvatar: user.avatar,
+      user_id: user.id,
+      username: user.username
+    }
   })
 }
 
@@ -177,8 +205,10 @@ module.exports = {
   cleanTables,
   makeGameArray,
   seedGames,
-  seedGamesAndUsers,
+  seedGamesUsers,
   makeSquadList,
   seedSquads,
-  seedGameSquadUsers
-};
+  seedGamesUsersSquads,
+  seedUserSquads,
+  makeExpectedSquadsList
+}
